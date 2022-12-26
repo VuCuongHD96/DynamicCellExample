1bc380be9dde7b2fcbe7d597e1c8404cf429b5f1
VuCuongHD96 <xuancuonghd1102@gmail.com>
2022-12-26 21:16:01 +0700

    Initial Commit

---
+++ DynamicCellExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicCellExample
+//
+//  Created by Work on 26/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
